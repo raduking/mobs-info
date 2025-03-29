649c498ebdb33eaa0ced978fbcde9e1bece3716d
Radu Sebastian LAZIN <raduking@gmail.com>
2025-03-29 13:09:27 +0200

    Moved client code to the MobsInfoClient class

--- src/main/java/org/raduking/mobsinfo/MobsInfo.java
+++ src/main/java/org/raduking/mobsinfo/MobsInfo.java
@@ -1,18 +1,11 @@
 package org.raduking.mobsinfo;
 
-import org.lwjgl.glfw.GLFW;
-import org.raduking.mobsinfo.hud.HudRenderer;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.fabric.api.client.rendering.v1.HudLayerRegistrationCallback;
-import net.fabricmc.fabric.api.client.rendering.v1.IdentifiedLayer;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
 
 public class MobsInfo implements ModInitializer {
 
@@ -20,35 +13,22 @@ public class MobsInfo implements ModInitializer {
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	private static final HudRenderer HUD_RENDERER = new HudRenderer();
-
-	private static KeyBinding toggleKeyBinding;
-	private static boolean modEnabled = true;
+	private static final AtomicBoolean MOD_ENABLED = new AtomicBoolean(true);
 
 	@Override
 	public void onInitialize() {
-		HudLayerRegistrationCallback.EVENT.register(layeredDrawer ->
-				layeredDrawer.attachLayerBefore(IdentifiedLayer.CHAT, HudRenderer.MOBS_INFO_LAYER, HUD_RENDERER::onHudRender));
-
-		// Bind the 'I' default key for enable/disable the mod.
-		toggleKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-				"mobsinfo.key.toggle",
-				InputUtil.Type.KEYSYM, // Type of key (keyboard key)
-				GLFW.GLFW_KEY_I, // Default key ('I' key)
-				"mobsinfo.category.keys"
-		));
-		ClientTickEvents.END_CLIENT_TICK.register(client -> {
-			while (toggleKeyBinding.wasPressed()) {
-				modEnabled = !modEnabled;
-				if (null != client.player) {
-					Text text = Text.translatable("mobsinfo.category.keys." + (modEnabled ? "enabled" : "disabled"));
-					client.player.sendMessage(text, false);
-				}
-			}
-		});
+		MobsInfo.LOGGER.info("Called MobsInfo.onInitialize");
 	}
 
 	public static boolean isModDisabled() {
-		return !modEnabled;
+		return !getModEnabled();
+	}
+
+	public static boolean getModEnabled() {
+		return MOD_ENABLED.get();
+	}
+
+	public static void setModEnabled(final boolean modEnabled) {
+		MobsInfo.MOD_ENABLED.set(modEnabled);
 	}
 }
--- src/main/java/org/raduking/mobsinfo/mixin/ExampleMixin.java
+++ src/main/java/org/raduking/mobsinfo/mixin/ExampleMixin.java
@@ -1,5 +1,6 @@
 package org.raduking.mobsinfo.mixin;
 
+import org.raduking.mobsinfo.MobsInfo;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,8 +10,10 @@ import net.minecraft.server.MinecraftServer;
 
 @Mixin(MinecraftServer.class)
 public class ExampleMixin {
+
 	@Inject(at = @At("HEAD"), method = "loadWorld")
-	private void init(CallbackInfo info) {
+	private void init(final CallbackInfo info) {
 		// This code is injected into the start of MinecraftServer.loadWorld()V
+		MobsInfo.LOGGER.info("Called ExampleMixin.init");
 	}
 }
